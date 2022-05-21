7dd80310ffb2c8aee3e1ec780deec618438a2c2f
ItsToxicGG <95104544+ItsToxicGG@users.noreply.github.com>
2022-05-22 01:00:01 +0200

    alr just wait ima

--- src/FaizDev/Tags/Main.php
+++ src/FaizDev/Tags/Main.php
@@ -71,7 +71,7 @@ class Main extends PluginBase implements Listener
 
 	public function onDataPacketSendEvent(DataPacketSendEvent $event) : void
 	{
-		$pk = $event::getNetworkSession()->getPacket();
+		$pk = $event::getPacket();
 		if ($pk instanceof TextPacket) {
 			$pattern = "/%*(([a-z0-9_]+\.)+[a-z0-9_]+)/i";
 			$replacement = "%$1";
